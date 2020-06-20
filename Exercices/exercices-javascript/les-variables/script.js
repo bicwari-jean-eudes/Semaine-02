66106a7873b4551b86447480f441b3c79d795290
Bicwari Jean Eudes <jeaneudes@siriusschool.be>
2020-06-20 11:31:13 +0200

    update

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,31 @@
+let salutation = 'hello world';
+console.log(salutation);
+let presentation = "bonjour je m'appelle";
+console.log(presentation);
+const prenom = 'Eudes';
+console.log(presentation + ' ' + prenom);
+let avoir = "j'ai";
+console.log(avoir);
+let nombre = '42';
+console.log(nombre);
+const année = 'ans';
+console.log(
+  presentation + ' ' + prenom + ' ' + avoir + ' ' + nombre + ' ' + année
+);
+let siriusStudentArray = [
+  'Eudes',
+  'Thierry',
+  'Eloi',
+  'Aline',
+  'Evelyne',
+  'Carene',
+  'Julie',
+  'Leila',
+  'Caleb',
+];
+console.log(siriusStudentArray[6]);
+console.log(siriusStudentArray[0]);
+console.log(siriusStudentArray);
+const student = { name: ' Bicwari', firstname: 'Jean Eudes', age: '42' };
+console.log(student.name);
+console.log(student.firstname);
