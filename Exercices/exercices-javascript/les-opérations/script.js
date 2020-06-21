e0ecb461845f4d0930227c8e2887753962dd87b1
Bicwari Jean Eudes <jeaneudes@siriusschool.be>
2020-06-21 07:33:42 +0200

    function

--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -1,4 +1,12 @@
-//Insérez ici les exercices 2.1 à 2.4
+/function carrée(a,a) { return a*a;};carrée(2,2);
+function carrée(a,a) { return a*a;};carrée(4,4);
+function carrée(a,a) { return a*a;};carrée(8,8);
+function carrée(a,a) { return a*a;};carrée(16,16);
+function modulo(a,b) { return a%b;};modulo(3,2);
+function modulo(a,b) { return a%b;};modulo(178,2);
+function modulo(a,b) { return a%b;};modulo(65,2);
+function modulo(a,b) { return a%b;};modulo(98,2);
+function modulo(a,b) { return a%b;};modulo(123,2);/Insérez ici les exercices 2.1 à 2.4
 
 // Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
 
@@ -13,7 +21,7 @@ function incrementOnClick() {
         // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
         let incrementCounter = 0;
 
-       console.log("Incrémentez") 
+       console.log("Incrémentez")
 })};
 // Exercice incrémentation
 function decrementOnClick() {
